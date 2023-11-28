f17b28863141d595c6b8d5d2a22385037a23b056
Malte Wiethoff <malte.wiethoff@gmail.com>
2023-11-28 17:01:45 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow *window;
+
+// Include GLM
+#include <glm/glm.hpp>
+
+// using namespace glm;
+using namespace std;
+
+int main()
+{
+    return 0;
+}
